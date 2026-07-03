dd223cec2d5a734eca8ae1dd11191894a93ef8e6
MadMelody <156449318+MadMelody@users.noreply.github.com>
2026-07-03 11:22:50 +0300

    Initial audio showcase upload
    Initial audio showcase upload

---
+++ tracks.js
@@ -0,0 +1,284 @@
+window.HIMFM_AUDIO_FOLDER = "OUT/";
+
+window.HIMFM_TRACKS = {
+  "themes": [
+    {
+      "number": "01",
+      "tracks": [
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 01_1_BASIC_ID.mp3"
+        },
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 01_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 01_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "BASIC ID 2",
+          "file": "ХИМFM_Джингл (вок)_Theme 01_2_BASIC_ID_2.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 01_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "02",
+      "tracks": [
+        {
+          "title": "BASIC ID 1",
+          "file": "ХИМFM_Джингл (вок)_Theme 02_1_BASIC_ID_1.mp3"
+        },
+        {
+          "title": "BASIC ID 2",
+          "file": "ХИМFM_Джингл (вок)_Theme 02_1_BASIC_ID_2.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 02_2_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "03",
+      "tracks": [
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 03_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 03_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 03_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "04",
+      "tracks": [
+        {
+          "title": "BASIC ID (Настроение отличного дня)",
+          "file": "ХИМFM_Джингл (вок)_Theme 04_BASIC_ID (Настроение отличного дня).mp3"
+        },
+        {
+          "title": "BASIC ID 2 (Настроение отличного дня)",
+          "file": "ХИМFM_Джингл (вок)_Theme 04_BASIC_ID_2 (Настроение отличного дня).mp3"
+        }
+      ]
+    },
+    {
+      "number": "05",
+      "tracks": [
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 05_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "SHOTGUN 2",
+          "file": "ХИМFM_Джингл (вок)_Theme 05_1_SHOTGUN_2.mp3"
+        },
+        {
+          "title": "BASIC ID (NEWS)",
+          "file": "ХИМFM_Джингл (вок)_Theme 05_2_BASIC_ID (NEWS).mp3"
+        },
+        {
+          "title": "BASIC ID (PROGRAM)",
+          "file": "ХИМFM_Джингл (вок)_Theme 05_2_BASIC_ID (PROGRAM).mp3"
+        }
+      ]
+    },
+    {
+      "number": "06",
+      "tracks": [
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 06_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 06_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 06_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "07",
+      "tracks": [
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 07_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 07_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 07_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "08",
+      "tracks": [
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 08_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 08_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 08_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "09",
+      "tracks": [
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 09_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 09_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "10",
+      "tracks": [
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 10_1_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 10_2_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "11",
+      "tracks": [
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 11_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 11_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "12",
+      "tracks": [
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 12_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 12_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 12_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "13",
+      "tracks": [
+        {
+          "title": "SHOTGUN",
+          "file": "ХИМFM_Джингл (вок)_Theme 13_1_SHOTGUN.mp3"
+        },
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 13_2_BASIC_ID.mp3"
+        }
+      ]
+    },
+    {
+      "number": "14",
+      "tracks": [
+        {
+          "title": "BASIC ID",
+          "file": "ХИМFM_Джингл (вок)_Theme 14_2_BASIC_ID.mp3"
+        },
+        {
+          "title": "RAMP (На одной волне)",
+          "file": "ХИМFM_Джингл (вок)_Theme 14_3_RAMP (На одной волне).mp3"
+        }
+      ]
+    },
+    {
+      "number": "15",
+      "tracks": [
+        {
+          "title": "BASIC ID (На одной волне)",
+          "file": "ХИМFM_Джингл (вок)_Theme 15_2_BASIC_ID (На одной волне).mp3"
+        },
+        {
+          "title": "RAMP",
+          "file": "ХИМFM_Джингл (вок)_Theme 15_3_RAMP.mp3"
+        }
+      ]
+    },
+    {
+      "number": "16",
+      "tracks": [
+        {
+          "title": "HOUR",
+          "file": "ХИМFM_Джингл (вок)_Theme 16_2_HOUR.mp3"
+        }
+      ]
+    }
+  ],
+  "vocalJingles": [
+    {
+      "title": "Круценко 01",
+      "file": "ХИМFM_Джингл (вок)_Круценко_01.mp3"
+    },
+    {
+      "title": "Круценко 03",
+      "file": "ХИМFM_Джингл (вок)_Круценко_03.mp3"
+    },
+    {
+      "title": "Круценко 04",
+      "file": "ХИМFM_Джингл (вок)_Круценко_04.mp3"
+    },
+    {
+      "title": "Круценко 05",
+      "file": "ХИМFM_Джингл (вок)_Круценко_05.mp3"
+    },
+    {
+      "title": "Круценко 06",
+      "file": "ХИМFM_Джингл (вок)_Круценко_06.mp3"
+    },
+    {
+      "title": "Круценко 07",
+      "file": "ХИМFM_Джингл (вок)_Круценко_07.mp3"
+    },
+    {
+      "title": "Круценко 08",
+      "file": "ХИМFM_Джингл (вок)_Круценко_08.mp3"
+    }
+  ]
+};
